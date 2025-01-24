c2cbe283ef8ba3f61f5adf434e840255c409449f
kevinpg93 <keeviin.93@gmail.com>
2025-01-24 19:04:43 +0000

    tu prima

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,43 +5,52 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuarios
+from sqlalchemy import select
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 db_url = os.getenv("DATABASE_URL")
+
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
 
+def handle_invalid_usage(error):
+
+    return jsonify(error.to_dict()), error.status_code
 # generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/usuarios', methods=['GET'])
 def handle_hello():
+    data = db.session.scalars(select(Usuarios)).all()
+    print(data)
+    # results = list(map(lambda item: item.serialize(),data))
+
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
-
+# @app.route('/personajes', methods=['GET'])
+# def get_personajes():
+#     response_body = {
+#         "msg": "Hello, this is your Character "
+#     }
+    return jsonify(response_body), 200
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,30 +1,70 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import Integer, String, Boolean
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
 db = SQLAlchemy()
 
-
-class User(db.Model):
-    __tablename__ = "user"
-
+class Usuarios(db.Model):
+    __tablename__ = "usuarios"
     id = mapped_column(Integer, primary_key=True)
-    email = mapped_column(String(120), nullable=False)
-    password = mapped_column(String(80))
-    is_active = mapped_column(Boolean)
+    nombre = mapped_column(String(50))
+    correo = mapped_column(String(100), nullable=False, unique = True)  
+    contrasena = mapped_column(String(20), nullable=False)
+    suscripcion = mapped_column(String(10))
+    favoritos: Mapped[List["Favoritos"]] = relationship()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+    #     return '<Usuarios %r>' % self.username
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "email": self.email,
+    #         # do not serialize the password, its a security breach
+    #     }
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planetas(db.Model):
+    __tablename__ = "planetas"
+    id = mapped_column(Integer, primary_key=True)
+    nombre_planeta = mapped_column(String(50), nullable=False)
+    poblacion = mapped_column(String(20), nullable=False)
+    extension = mapped_column(String(10), nullable=False)
+    favoritos: Mapped[List["Favoritos"]] = relationship()
+
+class Personajes(db.Model):
+    __tablename__ = "personajes"
+    id = mapped_column(Integer, primary_key=True)
+    nombre_personaje = mapped_column(String(50), nullable=False)
+    color_de_ojos = mapped_column(String(100), nullable=False)
+    color_de_pelo = mapped_column(String(20), nullable=False)
+    altura_de = mapped_column(String(10), nullable=False)
+    favoritos: Mapped[List["Favoritos"]] = relationship()
+
+class Favoritos(db.Model):
+    __tablename__ = "favoritos"
+    id = mapped_column(Integer, primary_key=True)
+    usuarios_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
+    planetas_id: Mapped[int] = mapped_column(ForeignKey("planetas.id"))
+    personajes_id: Mapped[int] = mapped_column(ForeignKey("personajes.id"))
 
 
-
+    
+# from flask_sqlalchemy import SQLAlchemy
+# db = SQLAlchemy()
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
 # from flask_sqlalchemy import SQLAlchemy
 
 # db = SQLAlchemy()
